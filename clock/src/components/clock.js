cee989f9340618fb5e7dcbe63571a6ad0e85d5e8
rickyg365 <rickygonzal3z@hotmail.com>
2022-01-25 14:51:23 -0800

    created functional version

--- clock/src/components/clock.js
+++ clock/src/components/clock.js
@@ -1,60 +1,123 @@
 import React, { useEffect, useState } from "react";
 import "./clock.css";
 
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date(), status: true };
-        this.start = this.start.bind(this);
-        this.stop = this.stop.bind(this);
-        this.toggleTimer = this.toggleTimer.bind(this);
-    }
+let Clock = () => {
+    // const [date, setDate] = useState(new Date());
+    // const [status, setStatus] = useState(true);
+    // let [timerID, setID] = useState(0);
 
-    componentDidMount() {
-        this.start();
-    }
+    // useEffect(() => {
+    //     setInterval(setDate(new Date()), 1000);
+    // }, []);
 
-    componentWillUnmount() {
-        this.start();
-        this.stop();
-    }
+    // useEffect(() => {
+    //     if (status) {
+    //         console.log("running");
+    //     } else {
+    //         console.log("stopped");
+    //     }
+    //     return () => {
+    //         console.log("unmount?");
+    //     };
+    // }, [status]);
 
-    start() {
-        this.tick();
-        this.timerID = setInterval(() => this.tick(), 1000);
-        this.setState({
-            status: true,
-        });
-    }
+    // let stop = () => {
+    //     clearInterval(timerID);
+    // };
 
-    tick() {
-        this.setState({ date: new Date() });
-    }
+    const [date, setDate] = useState(new Date());
+    const [status, setStatus] = useState(true);
 
-    stop() {
-        clearInterval(this.timerID);
-        this.setState({
-            status: false,
-        });
+    function refreshClock() {
+        setDate(new Date());
     }
-
-    toggleTimer() {
-        if (this.state.status) {
-            this.stop();
+    useEffect(() => {
+        const timerId = setInterval(refreshClock, 1000);
+        if (status) {
+            console.log("running");
+            console.log(timerId);
         } else {
-            this.start();
+            console.log("mid clear");
+            console.log("timer not running");
         }
-    }
+        return () => {
+            // Runs this First
+            console.log("final clear");
+            clearInterval(timerId);
+        };
+    }, [status]);
 
-    render() {
-        return (
-            <div className="clock-container" onClick={this.toggleTimer}>
-                <h2 className="clock-time">
-                    {this.state.date.toLocaleTimeString()}
-                </h2>
-            </div>
-        );
-    }
-}
+    let toggleStatus = () => {
+        if (status) {
+            setStatus(false);
+            console.log("set False");
+        } else {
+            setStatus(true);
+            console.log("set True");
+        }
+    };
+
+    return (
+        <div className="clock-container" onClick={toggleStatus}>
+            <h2 className="clock-time">{date.toLocaleTimeString()}</h2>
+        </div>
+    );
+};
+
+// class Clock extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { date: new Date(), status: true };
+//         this.start = this.start.bind(this);
+//         this.stop = this.stop.bind(this);
+//         this.toggleTimer = this.toggleTimer.bind(this);
+//     }
+
+//     componentDidMount() {
+//         this.start();
+//     }
+
+//     componentWillUnmount() {
+//         this.start();
+//         this.stop();
+//     }
+
+//     start() {
+//         this.tick();
+//         this.timerID = setInterval(() => this.tick(), 1000);
+//         this.setState({
+//             status: true,
+//         });
+//     }
+
+//     tick() {
+//         this.setState({ date: new Date() });
+//     }
+
+//     stop() {
+//         clearInterval(this.timerID);
+//         this.setState({
+//             status: false,
+//         });
+//     }
+
+//     toggleTimer() {
+//         if (this.state.status) {
+//             this.stop();
+//         } else {
+//             this.start();
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <div className="clock-container" onClick={this.toggleTimer}>
+//                 <h2 className="clock-time">
+//                     {this.state.date.toLocaleTimeString()}
+//                 </h2>
+//             </div>
+//         );
+//     }
+// }
 
 export default Clock;
